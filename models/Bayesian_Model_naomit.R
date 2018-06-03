82727d2834ed5c009322243e8435a970001b995b
mcooper <mw.coop.r@gmail.com>
2018-06-03 12:34:50 +0000

    write blobs better

--- models/Bayesian_Model_naomit.R
+++ models/Bayesian_Model_naomit.R
@@ -9,11 +9,11 @@ sc <- createAzureContext(tenantID = az$tenantid,
 
 az_read_csv <- function(csv, container="dhsprocessed"){
   read.csv(text=azureGetBlob(sc, 
-                            storageAccount=az$storage_acct, 
-                            container=container,
-                            blob=csv,
-                            type="text",
-                            storageKey = az$storageKey))
+                             storageAccount=az$storage_acct, 
+                             container=container,
+                             blob=csv,
+                             type="text",
+                             storageKey = az$storageKey))
 }
 
 az_write_blob <- function(blob, blobname, container='stan-models'){
@@ -25,38 +25,13 @@ az_write_blob <- function(blob, blobname, container='stan-models'){
 }
 
 hh <- az_read_csv('hhvars.csv')
-gdp <- az_read_csv('country_gdp.csv')
-farm <- az_read_csv('FarmingSystems.csv')
-md <- az_read_csv('MarketDist.csv')
-pop <- az_read_csv('PopPer100sqkm.csv')
 spi <- az_read_csv('Coords&Precip.csv')
-
-#################################
-#Process pop and market dist data
-#################################
-pop00 <- merge(select(pop, code, pop=pop00), data.frame(interview_year=seq(1988, 2002)))
-pop05 <- merge(select(pop, code, pop=pop05), data.frame(interview_year=seq(2003, 2007)))
-pop10 <- merge(select(pop, code, pop=pop10), data.frame(interview_year=seq(2008, 2012)))
-pop15 <- merge(select(pop, code, pop=pop15), data.frame(interview_year=seq(2013, 2017)))
-
-pop <- Reduce(bind_rows, list(pop00, pop05, pop10, pop15))
-
-
-md00 <- merge(select(md, code, md=market2000), data.frame(interview_year=seq(1988, 2007)))
-md15 <- merge(select(md, code, md=market2015), data.frame(interview_year=seq(2008, 2016)))
-
-md <- Reduce(bind_rows, list(md00, md15))
 
 ################################
 #Combine and clear workspace
 ################################
 all <- Reduce(function(x, y){merge(x, y, all.x=T, all.y=F)},
-              list(hh, gdp, farm, md, pop, spi))
-
-all$md <- all$md/(24*7)
-all$gdp <- all$gdp/1000
-all$mean_annual_precip <- all$mean_annual_precip/1000
-all$pop <- all$pop/100000
+              list(hh, spi))
 
 all <- all %>%
   filter(is_visitor == 0 & years_in_location >= 2)
@@ -142,10 +117,10 @@ flood <- sel %>%
 #plot(flood$longitude, flood$latitude, pch=16, cex=0.2)
 
 mod_d <- lmer(haz_dhs ~ spei24 + toilet + relationship_hhhead + age + birth_order + head_age + head_sex + hhsize + sex + wealth_index + diarrhea + fever + 
-               breast_duration + urban_rural + parents_years_ed + (1 | code), data=drought)
+                breast_duration + urban_rural + parents_years_ed + (1 | code), data=drought)
 
 mod_f <- lmer(haz_dhs ~ spei24  + toilet + relationship_hhhead + age + birth_order + head_age + head_sex + hhsize + sex + wealth_index + diarrhea + fever + 
-               breast_duration + urban_rural + parents_years_ed + (1 | code), data=flood)
+                breast_duration + urban_rural + parents_years_ed + (1 | code), data=flood)
 
 ############################################
 #Stan-tastic models
@@ -200,91 +175,94 @@ stanDat[["code"]] <- as.numeric(as.factor(as.character(drought$code)))
 
 drought_stan_code <- "
 data {
-  int<lower=1> N;
-  
-  real<lower=-600, upper=600> haz_dhs[N];
+int<lower=1> N;
 
-  int<lower=0, upper=1> toiletFlushToilet[N];
-  int<lower=0, upper=1> toiletOther[N];
-  int<lower=0, upper=1> toiletPitLatrine[N];
-  int<lower=0, upper=1> relationship_hhheadNotRelated[N];
-  int<lower=0, upper=1> relationship_hhheadRelative[N];
-  int<lower=0, upper=59> age[N];
-  int<lower=0, upper=18> birth_order[N];
-  int<lower=14, upper=97> head_age[N];
-  int<lower=0, upper=1> head_sexMale[N];
-  int<lower=0, upper=1> sexMale[N];
-  int<lower=0, upper=1> wealth_indexMiddle[N];
-  int<lower=0, upper=1> wealth_indexPoorer[N];
-  int<lower=0, upper=1> wealth_indexRicher[N];
-  int<lower=0, upper=1> wealth_indexRichest[N];
-  int<lower=2, upper=53> hhsize[N];
-  int<lower=0, upper=1> diarrhea[N];
-  int<lower=0, upper=1> fever[N];
-  int<lower=0, upper=59> breast_duration[N];
-  int<lower=0, upper=1> urban_ruralRural[N];
-  int<lower=0, upper=23> parents_years_ed[N];
-  //real<lower=0, upper=10> gdp[N];
-  //real<lower=0, upper=200> md[N];
-  //real<lower=0, upper=40000> pop[N];
-  //real<lower=0, upper=10> mean_annual_precip[N];
-  real<lower=-3, upper=3> spei24[N];
+real<lower=-600, upper=600> haz_dhs[N];
 
-  int<lower=1> code_N;       //number of sites
+int<lower=0, upper=1> toiletFlushToilet[N];
+int<lower=0, upper=1> toiletOther[N];
+int<lower=0, upper=1> toiletPitLatrine[N];
+int<lower=0, upper=1> relationship_hhheadNotRelated[N];
+int<lower=0, upper=1> relationship_hhheadRelative[N];
+int<lower=0, upper=59> age[N];
+int<lower=0, upper=18> birth_order[N];
+int<lower=14, upper=97> head_age[N];
+int<lower=0, upper=1> head_sexMale[N];
+int<lower=0, upper=1> sexMale[N];
+int<lower=0, upper=1> wealth_indexMiddle[N];
+int<lower=0, upper=1> wealth_indexPoorer[N];
+int<lower=0, upper=1> wealth_indexRicher[N];
+int<lower=0, upper=1> wealth_indexRichest[N];
+int<lower=2, upper=53> hhsize[N];
+int<lower=0, upper=1> diarrhea[N];
+int<lower=0, upper=1> fever[N];
+int<lower=0, upper=59> breast_duration[N];
+int<lower=0, upper=1> urban_ruralRural[N];
+int<lower=0, upper=23> parents_years_ed[N];
+//real<lower=0, upper=10> gdp[N];
+//real<lower=0, upper=200> md[N];
+//real<lower=0, upper=40000> pop[N];
+//real<lower=0, upper=10> mean_annual_precip[N];
+real<lower=-3, upper=3> spei24[N];
 
-  int<lower=1> code[N];      //site id
+int<lower=1> code_N;       //number of sites
+
+int<lower=1> code[N];      //site id
 
 }
 
 parameters {
-  real intercept;
-  real toiletFlushToilet_beta;
-  real toiletOther_beta;
-  real toiletPitLatrine_beta;
-  real relationship_hhheadNotRelated_beta;
-  real relationship_hhheadRelative_beta;
-  real age_beta;
-  real birth_order_beta;
-  real head_age_beta;
-  real head_sexMale_beta;
-  real sexMale_beta;
-  real wealth_indexMiddle_beta;
-  real wealth_indexPoorer_beta;
-  real wealth_indexRicher_beta;
-  real wealth_indexRichest_beta;
-  real hhsize_beta;
-  real diarrhea_beta;
-  real fever_beta;
-  real breast_duration_beta;
-  real urban_ruralRural_beta;
-  real parents_years_ed_beta;
-  real<lower=0> sigma_e; //error sd
+real intercept;
+real toiletFlushToilet_beta;
+real toiletOther_beta;
+real toiletPitLatrine_beta;
+real relationship_hhheadNotRelated_beta;
+real relationship_hhheadRelative_beta;
+real age_beta;
+real birth_order_beta;
+real head_age_beta;
+real head_sexMale_beta;
+real sexMale_beta;
+real wealth_indexMiddle_beta;
+real wealth_indexPoorer_beta;
+real wealth_indexRicher_beta;
+real wealth_indexRichest_beta;
+real hhsize_beta;
+real diarrhea_beta;
+real fever_beta;
+real breast_duration_beta;
+real urban_ruralRural_beta;
+real parents_years_ed_beta;
+real<lower=0> sigma_e; //error sd
 
-  vector<lower=0>[code_N] re_spei24_beta;
-  vector[code_N] re_intercept;
+vector<lower=0>[code_N] re_spei24_beta;
+vector[code_N] re_intercept;
 
-  real<lower=0> re_intercept_sigma;
-  real<lower=0> re_spei24_df;
+real<lower=0> re_intercept_sigma;
+real<lower=0> re_spei24_sigma;
+
+real<lower=0> re_spei24_mean;
 
 }
 
 
 model {
-  real mu;
+real mu;
 
-  sigma_e ~ cauchy(0, 2);
-  re_intercept_sigma ~ cauchy(0, 1);
-  re_spei24_df ~ cauchy(0, 1);
-  
-  re_intercept ~ normal(0, re_intercept_sigma);
-  re_spei24_beta ~ chi_square(re_spei24_df);
+sigma_e ~ cauchy(0, 2);
+re_intercept_sigma ~ cauchy(0, 1);
+re_spei24_sigma ~ cauchy(0, 1);
+re_spei24_mean ~ chi_square(1);
+
+re_intercept ~ normal(0, re_intercept_sigma);
+re_spei24_beta ~ normal(re_spei24_mean, re_spei24_sigma);
 
 
-  for (i in 1:N){
-    mu = intercept + re_intercept[code[i]] + re_spei24_beta[code[i]] * spei24[i] + toiletFlushToilet_beta*toiletFlushToilet[i] + toiletOther_beta*toiletOther[i] + toiletPitLatrine_beta*toiletPitLatrine[i] + relationship_hhheadNotRelated_beta*relationship_hhheadNotRelated[i] + relationship_hhheadRelative_beta*relationship_hhheadRelative[i] + age_beta*age[i] + birth_order_beta*birth_order[i] + head_age_beta*head_age[i] + head_sexMale_beta*head_sexMale[i] + sexMale_beta*sexMale[i] + wealth_indexMiddle_beta*wealth_indexMiddle[i] + wealth_indexPoorer_beta*wealth_indexPoorer[i] + wealth_indexRicher_beta*wealth_indexRicher[i] + wealth_indexRichest_beta*wealth_indexRichest[i] + hhsize_beta*hhsize[i] + diarrhea_beta*diarrhea[i] + fever_beta*fever[i] + breast_duration_beta*breast_duration[i] + urban_ruralRural_beta*urban_ruralRural[i] + parents_years_ed_beta*parents_years_ed[i];
+for (i in 1:N){
+mu = intercept + re_intercept[code[i]] + re_spei24_beta[code[i]] * spei24[i] + toiletFlushToilet_beta*toiletFlushToilet[i] + toiletOther_beta*toiletOther[i] + toiletPitLatrine_beta*toiletPitLatrine[i] + relationship_hhheadNotRelated_beta*relationship_hhheadNotRelated[i] + relationship_hhheadRelative_beta*relationship_hhheadRelative[i] + age_beta*age[i] + birth_order_beta*birth_order[i] + head_age_beta*head_age[i] + head_sexMale_beta*head_sexMale[i] + sexMale_beta*sexMale[i] + wealth_indexMiddle_beta*wealth_indexMiddle[i] + wealth_indexPoorer_beta*wealth_indexPoorer[i] + wealth_indexRicher_beta*wealth_indexRicher[i] + wealth_indexRichest_beta*wealth_indexRichest[i] + hhsize_beta*hhsize[i] + diarrhea_beta*diarrhea[i] + fever_beta*fever[i] + breast_duration_beta*breast_duration[i] + urban_ruralRural_beta*urban_ruralRural[i] + parents_years_ed_beta*parents_years_ed[i];
 
-    haz_dhs[i] ~ normal(mu, sigma_e);
-  }
+haz_dhs[i] ~ normal(mu, sigma_e);
+}
 }
 "
 
@@ -292,18 +270,16 @@ model {
 stanmod <- stan(model_name="mode1", model_code = drought_stan_code, data=stanDat,
                 iter = 2000, chains = 4, init=init)
 
-az_write_blob(stanDat, '2018-05-21-ChiSqRE-data')
-az_write_blob(drought_stan_code, '2018-05-21-ChiSqRE-code')
-az_write_blob(stanmod, '2018-05-21-ChiSqRE-results')
+time <- substr(Sys.time(), 1, 10)
 
+modtitle <- 'RE_mean_chiSq'
+
+az_write_blob(list(stanDat, drought_stan_code, stanmod), paste0(time, modtitle))
 
 sum <- summary(stanmod)$summary
 
-w1 <- sum[grepl('w[1', row.names(sum), fixed=T), 'mean']
-w2 <- sum[grepl('w[2', row.names(sum), fixed=T), 'mean']
-
-codemap$w2 <- w2
-codemap$w1 <- w1
+spei24 <- sum[grepl('re_spei24_beta', row.names(sum), fixed=T), 'mean']
+intercept <- sum[grepl('re_intercept[', row.names(sum), fixed=T), 'mean']
 
 sel <- sum[ , 'mean']
 
