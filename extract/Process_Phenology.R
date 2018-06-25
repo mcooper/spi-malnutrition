ec9c5575930fbd8997a345925fdf8ad48563b3cf
mcooper <mw.coop.r@gmail.com>
2018-06-25 15:02:32 +0200

    process phenology data

--- extract/Process_Phenology.R
+++ extract/Process_Phenology.R
@@ -1,13 +1,6 @@
-setwd('G://My Drive/DHS Processed')
-
-library(dplyr)
-
-sp <- read.csv('sp_export.csv') %>%
-  select(code, latitude, longitude) %>%
-  unique
-
 setwd('G://My Drive/DHS Spatial Covars/Phenology')
 
+library(dplyr)
 library(raster)
 library(rgdal)
 
@@ -15,12 +8,13 @@ start1 <- raster('phenos1_v02.tif')
 start2 <- raster('phenos2_v02.tif')
 
 end1 <- raster('phenoe1_v02.tif')
-end2 <- raster('phenoe1_v02.tif')
+end2 <- raster('phenoe2_v02.tif')
 
 ref <- raster('../../CHIRPS/Monthly/chirps-v2.0.1981.01.tif')
 
-start1res <- resample(start1, ref, method="bilinear", filename="start1aggregate.tif", format='GTiff')
-start2res <- resample(start2, ref, method="bilinear", filename="start2aggregate.tif", format='GTiff')
+start1res <- resample(start1, ref, method="bilinear")#, filename="start1aggregate.tif", format='GTiff')
+start2res <- resample(start2, ref, method="bilinear")#, filename="start2aggregate.tif", format='GTiff')
 
-end1res <- resample(end1, ref, method="bilinear", filename="end1aggregate.tif", format='GTiff')
-end2res <- resample(end2, ref, method="bilinear", filename="end2aggregate.tif", format='GTiff')
+end1res <- resample(end1, ref, method="bilinear")#, filename="end1aggregate.tif", format='GTiff')
+end2res <- resample(end2, ref, method="bilinear")#, filename="end2aggregate.tif", format='GTiff')
+
