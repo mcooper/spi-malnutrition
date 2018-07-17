3d4457e7ed3ce73d1a9a1775f11b8ad765960838
mcooper <mw.coop.r@gmail.com>
2018-07-17 15:44:48 +0200

    First INLA model!

--- models/Lmer predicted raster.R
+++ models/Lmer predicted raster.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(lme4)
 library(broom)
 
-setwd('G://My Drive/DHS Spatial Covars/Final Rasters/')
+setwd('G://My Drive/DHS Processed')
 
 hha <- read.csv('HH_data_A.csv')
 lc <- read.csv('landcover_processed.csv')
@@ -19,13 +19,39 @@ mod <- lmer(haz_dhs ~ interview_year + (1|country/interview_month) + age + birth
 
 all$residuals <- residuals(mod)
 
-mod2 <- lm(residuals ~ spei24*market_dist + spei24*forest + spei24*bare + spei24*market_dist + spei24*ag_pct_gdp + 
+mod2 <- lm(residuals ~ spei24*market_dist + spei24*forest + spei24*bare + spei24*ag_pct_gdp + 
              spei24*precip_10yr_mean + spei24*gdp + spei24*government_effectiveness + spei24*irrigation + 
              spei24*ndvi + spei24*population + spei24*stability_violence + spei24*tmax_10yr_mean + spei24*tmin_10yr_mean + 
-             spei24*crop_prod + spei24*fieldsize + spei24*nutritiondiversity + urban_rural, data=all)
+             spei24*fieldsize + spei24*nutritiondiversity, data=all)
 
 library(raster)
 
-setwd()
+setwd('G://My Drive/DHS Spatial Covars/Final Rasters')
 
-s <- stack(list.files(pattern='*.tif$'))
+market_dist <- raster('market_distance.tif')
+forest <- raster('forest.tif')
+bare <- raster('bare.tif')
+ag_pct_gdp <- raster('ag_pct_gdp.tif')
+precip_10yr_mean <- raster('CHIRPS_10yr_avg.tif')
+gdp <- raster('gdp2020.tif')
+govt_eff <- raster('government_effectiveness.tif')
+irrigation <- raster('irrigation.tif')
+ndvi <- raster('ndvi.tif')
+pop <- raster('population.tif')
+sv <- raster('stability_violence.tif')
+tmax <- raster('TMAX_10yr_avg.tif')
+tmin <- raster('TMIN_10yr_avg.tif')
+field_size <- raster('fieldsize.tif')
+nd <- raster('nutritiondiversity.tif')
+
+f <- -1.601e-05*market_dist + -4.119e-05*forest + 4.050e-04*bare -4.354e-04*ag_pct_gdp + 5.979e-05*precip_10yr_mean + 1.130e-05*gdp + 2.208e-04*govt_eff + 
+  1.556e-04*irrigation + 2.909e-06*ndvi + 4.837e-07*pop + 1.285e-03*sv + 1.096e-02*tmax -6.328e-03*tmin + 1.339e-03*field_size -9.379e-02*nd
+
+
+
+
+
+
+
+
+
