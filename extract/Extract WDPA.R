12d73d290ad915d5d0ebaf05cfd5d5423cb7ee87
mcooper <mw.coop.r@gmail.com>
2018-08-01 13:36:36 +0200

    paralellize WDPA extraction

--- extract/Extract WDPA.R
+++ extract/Extract WDPA.R
@@ -2,32 +2,36 @@ library(sp)
 library(rgeos)
 library(rgdal)
 library(dplyr)
+library(foreach)
+library(doParallel)
 
-wdpa <- readOGR('G://My Drive/WDPA', 'WDPA_July2018-shapefile-polygons',
+wdpa <- readOGR('.', 'WDPA_Aug2018-shapefile-polygons',
                 stringsAsFactors=F)
-
-wdpa <- wdpa[wdpa$IUCN_CAT %in% c('Ia', 'Ib', 'II', 'III', 'IV', 'V', 'VI') & wdpa$Status_YR != 0, ]
 
 wdpa <- spTransform(wdpa, CRS("+proj=aeqd +lat_0=0 +lon_0=0"))
 
-sp <- read.csv('G://My Drive/DHS Processed/sp_export.csv') %>%
+wdpa@data$STATUS_YR <- as.numeric(wdpa@data$STATUS_YR)
+
+wdpa <- wdpa[wdpa$IUCN_CAT %in% c('Ia', 'Ib', 'II', 'III', 'IV', 'V', 'VI') & wdpa$STATUS_YR != 0, ]
+
+sp <- read.csv('~/sp_export.csv') %>%
   select(latitude, longitude, code, interview_year) %>%
   unique
 
 sp <- SpatialPointsDataFrame(sp[ , c('longitude', 'latitude')], sp, proj4string = CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'))
 
-n <- length(sp)
-nearestPaClass <- character(n)
-nearestPaDist <- character(n)
+sp <- spTransform(sp, CRS("+proj=aeqd +lat_0=0 +lon_0=0"))
 
-#TODO: Paralellize and run in cloud
-wdpa$STATUS_YR <- as.numeric(wdpa$STATUS_YR)
+cl <- makeCluster(detectCores(), outfile = '')
+registerDoParallel(cl)
 
 for (i in unique(sp$interview_year)){
-  wdpa_sel <- wdpa[as.numeric(wdpa$STATUS_YR) <= i, ]
+  print(i)
+  
+  wdpa_sel <- wdpa[wdpa$STATUS_YR <= i, ]
   sp_sel <- sp[sp$interview_year == i, ]
   
-  for (j in seq_along(sp_sel)){
+  foreach(j=seq_along(sp_sel), .packages=c('sp', 'rgeos', 'rgdal', 'dplyr')) %dopar% {
     code <- sp_sel@data[j, 'code']
     year <- sp_sel@data[j, 'interview_year']
     
@@ -36,9 +40,9 @@ for (i in unique(sp$interview_year)){
     nearestPaID <- wdpa_sel$WDPAID[which.min(dists)]
     nearestPaDist <- min(dists)
     
-    sp$nearestPaClass[sp$code==code & sp$interview_year==year] <- nearestPaClass
-    sp$nearestPaID[sp$code==code & sp$interview_year==year] <- nearestPaID
-    sp$nearestPaDist[sp$code==code & sp$interview_year==year] <- nearestPaDist
+    df <- data.frame(code, year, nearestPaClass, nearestPaID, nearestPaDist)
+    
+    write.csv(df, '~/WDPAoutputs/', row.names=F)
   }
 }
 
