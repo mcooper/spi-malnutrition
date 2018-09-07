e678037a57c2d22670543692491de995d99a4270
mcooper <mw.coop.r@gmail.com>
2018-09-06 21:01:12 -0400

    final model for IIASA report

--- models/Lmer predicted raster expand.grid.R
+++ models/Lmer predicted raster expand.grid.R
@@ -2,8 +2,9 @@ library(dplyr)
 library(broom)
 library(foreach)
 library(doParallel)
+library(MASS)
 
-#setwd('~/dhsprocessed')
+setwd('~/dhsprocessed')
 setwd('G://My Drive/DHS Processed')
 
 hha <- read.csv('HH_data_A.csv')
@@ -13,9 +14,11 @@ cov <- read.csv('SpatialCovars.csv')
 all <- Reduce(function(x, y){merge(x,y,all.x=T, all.y=F)}, list(hha, spei, cov)) %>%
   na.omit
 
+
 #Try rescaling some geospatial covariates
 all$precip_10yr_mean <- (all$precip_10yr_mean*12)/1000
 all$gdp <- all$gdp/1000
+all$grid_gdp <- all$grid_gdp/1000
 all$market_dist <- all$market_dist/(24*7)
 all$ndvi <- all$ndvi/5000
 all$population <- all$population/1000
@@ -23,48 +26,52 @@ all$tmax_10yr_mean <- all$tmax_10yr_mean - 273.15
 all$tmin_10yr_mean <- all$tmin_10yr_mean - 273.15
 all$builtup <- all$builtup*100
 all$elevation <- all$elevation/1000
+all$imports_percap <- all$imports_percap/1000
 
 all$gdp_l <- log(all$gdp)
+all$grid_gdp_l <- log(all$grid_gdp)
 
 #exclude:
-popbuilt <- c(' + spei*population', 
-              ' + spei*builtup', 
-              ' + spei*population + spei*builtup', 
-              ' + spei*low_settle', 
-              ' + spei*high_settle')
+popbuilt <- paste0(' + spei*',
+            c('population', 'builtup', 'low_settle', 'high_settle'))
 
-roughness <- c(' + spei*roughness', 
-               ' + spei*elevation', 
-               ' + spei*roughness + spei*elevation')
+roughness <- paste0(' + spei*',
+                   c('roughness', 
+                     'elevation', 
+                     'tmax_10yr_mean'))
 
-es <- c(' + spei*ndvi', 
-        ' + spei*forest', 
-        ' + spei*ndvi + spei*forest')
+es <- paste0(' + spei*',
+             c('ndvi', 
+              'forest', 
+              'bare',
+              'precip_10yr_mean'))
 
-wdi <- c(' + spei*government_effectiveness', 
-         ' + spei*stability_violence', 
-         ' + spei*government_effectiveness + spei*stability_violence')
+gdp <- paste0(' + spei*',
+              c("ag_pct_gdp",
+                "gdp",
+                "gdp_l", 
+                "grid_gdp_l",  
+                "grid_gdp",  
+                "grid_hdi",  
+                "imports_percap"))
 
-ag <- c(' + spei*ag_pct_gdp', '')
-
-#transform
-gdp <- c(' + spei*gdp', ' + spei*gdp_l')
+md <- c(' + spei*market_dist', '')
 
 #cutoffs:
 cutoffs <- c('1', '2', '1.5', 'loess')
 
 spei <- c('spei24', 'spei36gs')
 
-gd <- expand.grid(popbuilt, roughness, es, wdi, gdp, cutoffs, spei, ag,
+gd <- expand.grid(popbuilt, roughness, es, gdp, md, cutoffs, spei,
                   stringsAsFactors = FALSE)
 
-names(gd) <- c('popbuilt', 'roughness', 'es', 'wdi',
-               'gdp', 'cutoffs', 'spei', 'ag')
+names(gd) <- c('popbuilt', 'roughness', 'es',
+               'gdp', 'md', 'cutoffs', 'spei')
 
-cl <- makeCluster(64, outfile = '')
+cl <- makeCluster(2, outfile = '')
 registerDoParallel(cl)
 
-foreach(i=1:nrow(gd), .packages=c('dplyr', 'broom')) %dopar% {
+foreach(i=1:nrow(gd), .packages=c('dplyr', 'broom', 'MASS')) %dopar% {
   
   df <- gd[i, ]
   
@@ -72,10 +79,9 @@ foreach(i=1:nrow(gd), .packages=c('dplyr', 'broom')) %dopar% {
                birth_order + hhsize + sex + mother_years_ed + toilet +
                head_age + head_sex + wealth_index + 
                spei*crop_prod + spei*irrigation + spei*nutritiondiversity +
-               spei*precip_10yr_mean + spei*tmax_10yr_mean"
+               spei*government_effectiveness + spei*stability_violence"
   
-  form <- paste0(form, gd$popbuilt[i], gd$roughness[i], gd$es[i], gd$wdi[i], gd$gdp[i],
-                 gd$ag[i])
+  form <- paste0(form, gd$popbuilt[i], gd$roughness[i], gd$es[i], gd$gdp[i], gd$md[i])
   
   all$spei <- all[ , gd$spei[i]]
   
@@ -101,96 +107,21 @@ foreach(i=1:nrow(gd), .packages=c('dplyr', 'broom')) %dopar% {
       relevel(ref = "Normal")
   }
   
-  mod <- lm(as.formula(form), data=all)
+  mod <- rlm(as.formula(form), data=all)
   
   df[ , 'AIC'] <- AIC(mod)
   
   s <- tidy(mod)
   row.names(s) <- s$term
-
-  #Wet Coefs
-  df[ , 'Wet_estimate'] <- s['speiWet', 'estimate']
-  df[ , 'Wet:ag_pct_gdp_estimate'] <- s['speiWet:ag_pct_gdp', 'estimate'] 
-  df[ , 'Wet:builtup_estimate'] <- s['speiWet:builtup', 'estimate']
-  df[ , 'Wet:crop_prod_estimate'] <- s['speiWet:crop_prod', 'estimate']
-  df[ , 'Wet:elevation_estimate'] <- s['speiWet:elevation', 'estimate']
-  df[ , 'Wet:forest_estimate'] <- s['speiWet:forest', 'estimate']
-  df[ , 'Wet:gdp_estimate'] <- s['speiWet:gdp', 'estimate']
-  df[ , 'Wet:gdp_l_estimate'] <- s['speiWet:gdp_l', 'estimate']
-  df[ , 'Wet:government_effectiveness_estimate'] <- s['speiWet:government_effectiveness', 'estimate']
-  df[ , 'Wet:high_settle_estimate'] <- s['speiWet:high_settle', 'estimate']
-  df[ , 'Wet:irrigation_estimate'] <- s['speiWet:irrigation', 'estimate']
-  df[ , 'Wet:low_settle_estimate'] <- s['speiWet:low_settle', 'estimate']
-  df[ , 'Wet:ndvi_estimate'] <- s['speiWet:ndvi', 'estimate']
-  df[ , 'Wet:nutritiondiversity_estimate'] <- s['speiWet:nutritiondiversity', 'estimate']
-  df[ , 'Wet:population_estimate'] <- s['speiWet:population', 'estimate']
-  df[ , 'Wet:precip_10yr_mean_estimate'] <- s['speiWet:precip_10yr_mean', 'estimate']
-  df[ , 'Wet:roughness_estimate'] <- s['speiWet:roughness', 'estimate']
-  df[ , 'Wet:stability_violence_estimate'] <- s['speiWet:stability_violence', 'estimate']
-  df[ , 'Wet:tmax_estimate'] <- s['speiWet:tmax', 'estimate']
   
-  #Wet Statistic
-  df[ , 'Wet_statistic'] <- s['speiWet', 'statistic']
-  df[ , 'Wet:ag_pct_gdp_statistic'] <- s['speiWet:ag_pct_gdp', 'statistic'] 
-  df[ , 'Wet:builtup_statistic'] <- s['speiWet:builtup', 'statistic']
-  df[ , 'Wet:crop_prod_statistic'] <- s['speiWet:crop_prod', 'statistic']
-  df[ , 'Wet:elevation_statistic'] <- s['speiWet:elevation', 'statistic']
-  df[ , 'Wet:forest_statistic'] <- s['speiWet:forest', 'statistic']
-  df[ , 'Wet:gdp_statistic'] <- s['speiWet:gdp', 'statistic']
-  df[ , 'Wet:gdp_l_statistic'] <- s['speiWet:gdp_l', 'statistic']
-  df[ , 'Wet:government_effectiveness_statistic'] <- s['speiWet:government_effectiveness', 'statistic']
-  df[ , 'Wet:high_settle_statistic'] <- s['speiWet:high_settle', 'statistic']
-  df[ , 'Wet:irrigation_statistic'] <- s['speiWet:irrigation', 'statistic']
-  df[ , 'Wet:low_settle_statistic'] <- s['speiWet:low_settle', 'statistic']
-  df[ , 'Wet:ndvi_statistic'] <- s['speiWet:ndvi', 'statistic']
-  df[ , 'Wet:nutritiondiversity_statistic'] <- s['speiWet:nutritiondiversity', 'statistic']
-  df[ , 'Wet:population_statistic'] <- s['speiWet:population', 'statistic']
-  df[ , 'Wet:precip_10yr_mean_statistic'] <- s['speiWet:precip_10yr_mean', 'statistic']
-  df[ , 'Wet:roughness_statistic'] <- s['speiWet:roughness', 'statistic']
-  df[ , 'Wet:stability_violence_statistic'] <- s['speiWet:stability_violence', 'statistic']
-  df[ , 'Wet:tmax_statistic'] <- s['speiWet:tmax', 'statistic']
+  s <- s[grepl('spei', s$term), ]
   
-  #Dry Coefs
-  df[ , 'Dry_estimate'] <- s['speiDry', 'estimate']
-  df[ , 'Dry:ag_pct_gdp_estimate'] <- s['speiDry:ag_pct_gdp', 'estimate'] 
-  df[ , 'Dry:builtup_estimate'] <- s['speiDry:builtup', 'estimate']
-  df[ , 'Dry:crop_prod_estimate'] <- s['speiDry:crop_prod', 'estimate']
-  df[ , 'Dry:elevation_estimate'] <- s['speiDry:elevation', 'estimate']
-  df[ , 'Dry:forest_estimate'] <- s['speiDry:forest', 'estimate']
-  df[ , 'Dry:gdp_estimate'] <- s['speiDry:gdp', 'estimate']
-  df[ , 'Dry:gdp_l_estimate'] <- s['speiDry:gdp_l', 'estimate']
-  df[ , 'Dry:government_effectiveness_estimate'] <- s['speiDry:government_effectiveness', 'estimate']
-  df[ , 'Dry:high_settle_estimate'] <- s['speiDry:high_settle', 'estimate']
-  df[ , 'Dry:irrigation_estimate'] <- s['speiDry:irrigation', 'estimate']
-  df[ , 'Dry:low_settle_estimate'] <- s['speiDry:low_settle', 'estimate']
-  df[ , 'Dry:ndvi_estimate'] <- s['speiDry:ndvi', 'estimate']
-  df[ , 'Dry:nutritiondiversity_estimate'] <- s['speiDry:nutritiondiversity', 'estimate']
-  df[ , 'Dry:population_estimate'] <- s['speiDry:population', 'estimate']
-  df[ , 'Dry:precip_10yr_mean_estimate'] <- s['speiDry:precip_10yr_mean', 'estimate']
-  df[ , 'Dry:roughness_estimate'] <- s['speiDry:roughness', 'estimate']
-  df[ , 'Dry:stability_violence_estimate'] <- s['speiDry:stability_violence', 'estimate']
-  df[ , 'Dry:tmax_estimate'] <- s['speiDry:tmax', 'estimate']
+  s$term <- gsub('spei', '', s$term)
   
-  #Dry Statistic
-  df[ , 'Dry_statistic'] <- s['speiDry', 'statistic']
-  df[ , 'Dry:ag_pct_gdp_statistic'] <- s['speiDry:ag_pct_gdp', 'statistic'] 
-  df[ , 'Dry:builtup_statistic'] <- s['speiDry:builtup', 'statistic']
-  df[ , 'Dry:crop_prod_statistic'] <- s['speiDry:crop_prod', 'statistic']
-  df[ , 'Dry:elevation_statistic'] <- s['speiDry:elevation', 'statistic']
-  df[ , 'Dry:forest_statistic'] <- s['speiDry:forest', 'statistic']
-  df[ , 'Dry:gdp_statistic'] <- s['speiDry:gdp', 'statistic']
-  df[ , 'Dry:gdp_l_statistic'] <- s['speiDry:gdp_l', 'statistic']
-  df[ , 'Dry:government_effectiveness_statistic'] <- s['speiDry:government_effectiveness', 'statistic']
-  df[ , 'Dry:high_settle_statistic'] <- s['speiDry:high_settle', 'statistic']
-  df[ , 'Dry:irrigation_statistic'] <- s['speiDry:irrigation', 'statistic']
-  df[ , 'Dry:low_settle_statistic'] <- s['speiDry:low_settle', 'statistic']
-  df[ , 'Dry:ndvi_statistic'] <- s['speiDry:ndvi', 'statistic']
-  df[ , 'Dry:nutritiondiversity_statistic'] <- s['speiDry:nutritiondiversity', 'statistic']
-  df[ , 'Dry:population_statistic'] <- s['speiDry:population', 'statistic']
-  df[ , 'Dry:precip_10yr_mean_statistic'] <- s['speiDry:precip_10yr_mean', 'statistic']
-  df[ , 'Dry:roughness_statistic'] <- s['speiDry:roughness', 'statistic']
-  df[ , 'Dry:stability_violence_statistic'] <- s['speiDry:stability_violence', 'statistic']
-  df[ , 'Dry:tmax_statistic'] <- s['speiDry:tmax', 'statistic']
+  for (j in 1:nrow(s)){
+    df[ , paste0(s$term[j], '_est')] <- s[j, 'estimate']
+    df[ , paste0(s$term[j], '_stat')] <- s[j, 'statistic']
+  }
   
   write.csv(df, paste0('~/mod_outputs/', i), row.names=F)
   
--- models/Lmer predicted raster.R
+++ models/Lmer predicted raster.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(lme4)
 library(broom)
 library(car)
+library(MASS)
 
 setwd('~/dhsprocessed')
 setwd('G://My Drive/DHS Processed')
@@ -16,6 +17,7 @@ all <- Reduce(function(x, y){merge(x,y,all.x=T, all.y=F)}, list(hha, spei, cov))
 #Try rescaling some geospatial covariates
 all$precip_10yr_mean <- (all$precip_10yr_mean*12)/1000
 all$gdp <- all$gdp/1000
+all$grid_gdp <- all$grid_gdp/1000
 all$market_dist <- all$market_dist/(24*7)
 all$ndvi <- all$ndvi/5000
 all$population <- all$population/1000
@@ -25,33 +27,9 @@ all$builtup <- all$builtup*100
 all$elevation <- all$elevation/1000
 all$imports_percap <- all$imports_percap/1000
 
-#Inverse Hyperbolic Sine Transformation
-#http://worthwhile.typepad.com/worthwhile_canadian_initi/2011/07/a-rant-on-inverse-hyperbolic-sine-transformations.html
-#
-ihs <- function(x) {
-  y <- log(x + sqrt(x ^ 2 + 1))
-  return(y)
-}
-
-
-#'Collinearity with:
-#' NDVI and Bare -0.9
-#' population and builtup 0.77
-#' tmax and tmin 0.82
-#' Forest and NDVI - 0.697
-#' AgGDP and GDP - 0.63
-#' NDVI and precip - 0.52
-#' NDVI and Forest - 0.58
-#' Forest and Bare - 0.51
-
-all$forest_ihs <- ihs(all$forest)
 all$gdp_l <- log(all$gdp)
-all$gdp_ihs <- ihs(all$gdp)
-#all$market_dist <- ihs(all$market_dist)
-all$population_ihs <- ihs(all$population)
-all$fieldsize_ihs <- ihs(all$fieldsize)
-all$builtup_ihs <- ihs(all$builtup)
-all$elevation_ihs <- ihs(all$elevation)
+all$grid_gdp_l <- log(all$grid_gdp)
+
 
 library(raster)
 
@@ -64,6 +42,8 @@ setNAs <- function(raster, column){
 }
 
 ag_pct_gdp <- raster('ag_pct_gdp.tif') #%>% setNAs('ag_pct_gdp')
+
+background <- ag_pct_gdp/ag_pct_gdp
 
 builtup <- (raster('builtup.tif')*100) #%>% setNAs('builtup')
 
@@ -78,13 +58,21 @@ forest <- raster('forest.tif')
 gdp <- (raster('gdp2020.tif')/1000) %>%
   setNAs('gdp')
 
+base <- gdp/gdp
+
 gdp_l <- log(gdp)
+
+grid_gdp <- (raster('grid_gdp.tif')/1000)
+
+grid_gdp_l <- log(grid_gdp)
+
+grid_hdi <- raster('grid_hdi.tif')
 
 government_effectiveness <- raster('government_effectiveness.tif') #%>% setNAs('government_effectiveness')
 
 high_settle <- raster('high_settle.tif') #%>% setNAs('high_settle')
 
-imports_percap <- raster('imports_percap.tif')
+imports_percap <- raster('imports_percap.tif')/1000
 
 irrigation <- raster('irrigation.tif') #%>% setNAs('irrigation')
 
@@ -106,95 +94,107 @@ stability_violence <- raster('stability_violence.tif') #%>% setNAs('stability_vi
 
 tmax_10yr_mean <- (raster('TMAX_10yr_avg.tif') - 273.15) #%>% setNAs('tmax_10yr_mean')
 
-makeRasts <- function(mod){
+makeRasts <- function(mod, speicat, usebase=FALSE){
   s <- tidy(mod)
   
-  #Make wet raster
-  wet_coefs <- s[grepl('speiWet', s$term), c('term', 'estimate')]
-  dry_coefs <- s[grepl('speiDry', s$term), c('term', 'estimate')]
+  term <- paste0('spei', speicat)
   
-  wet_coefs$term <- gsub('speiWet:', '', wet_coefs$term)
-  dry_coefs$term <- gsub('speiDry:', '', dry_coefs$term)
+  coefs <- s[grepl(term, s$term), c('term', 'estimate')]
   
-  wet <- wet_coefs$estimate[1]
-  for (i in 2:nrow(wet_coefs)){
-    rast <- get(wet_coefs$term[i])
-    wet <- wet + rast*wet_coefs$estimate[i]
+  coefs$term <- gsub(paste0(term, ':'), '', coefs$term)
+  
+  if (usebase){
+    rast <- base*coefs$estimate[1]
+  } else{
+    rast <- coefs$estimate[1]
+  }
+  for (i in 2:nrow(coefs)){
+    tmp_rast <- get(coefs$term[i])
+    suppressWarnings(rast <- rast + tmp_rast*coefs$estimate[i])
   }
   
-  dry <- dry_coefs$estimate[1]
-  for (i in 2:nrow(dry_coefs)){
-    rast <- get(dry_coefs$term[i])
-    dry <- dry + rast*dry_coefs$estimate[i]
-  }
+  rast_censored <- rast
+  rast_censored[rast > 0] <- NA
   
-  wet_censored <- wet
-  wet_censored[wet > 0] <- NA
-  
-  dry_censored <- dry
-  dry_censored[dry > 0] <- NA
-  
-  return(list(wet, wet_censored, dry, dry_censored))
+  return(list(rast, rast_censored))
 }
 
 #Make categorical
-all$spei <- all$spei36gs
+all$spei <- all$spei24
 
 all$spei <- ifelse(all$spei > 1.5, "Wet",
                    ifelse(all$spei < -0.4, "Dry", "Normal")) %>%
   as.factor %>%
   relevel(ref = "Normal")
 
-
-mod <- lm(haz_dhs ~ interview_year + age + urban_rural + 
+mod <- lm(haz_dhs ~ age + as.factor(calc_birthmonth) + 
             birth_order + hhsize + sex + mother_years_ed + toilet +
-            head_age + head_sex + wealth_index + spei + 
-               #spei*ag_pct_gdp +
-               spei*builtup +
-               spei*crop_prod +
-               spei*elevation +
-               spei*forest +
-               spei*gdp_l +
-               spei*government_effectiveness +
-               #spei*high_settle + 
-               #spei*imports_percap + 
-               spei*irrigation +
-               #spei*low_settle + 
-               spei*market_dist +
-               #spei*ndvi +
-               #spei*bare + 
-               spei*nutritiondiversity +
-               spei*population +
-               spei*precip_10yr_mean +
-               spei*roughness + 
-               spei*stability_violence +
-               spei*tmax_10yr_mean
-             , data=all)
+            head_age + head_sex + wealth_index + 
+            
+            #GDP related vars
+            #spei*ag_pct_gdp +
+            #spei*gdp_l +
+            #spei*grid_gdp_l + 
+            spei*grid_gdp + 
+            #spei*grid_hdi + 
+            #spei*imports_percap + 
+            
+            #Pop-Urban vars
+            #spei*builtup +
+            #spei*low_settle + #Slightly colinear with irrigation (0.50) so avoid this one
+            #spei*high_settle + 
+            spei*population +
+            
+            #Land Cover Vars
+            #spei*precip_10yr_mean +
+            #spei*forest +
+            spei*ndvi +
+            #spei*bare + 
+            
+            #Topographic Vars
+            #spei*elevation +
+            #spei*roughness + 
+            spei*tmax_10yr_mean +
+          
+            #Totally Independat Vars
+            spei*market_dist + #not the same before and after 2000
+            #spei*fieldsize + 
+            spei*crop_prod +
+            spei*government_effectiveness +
+            spei*irrigation +
+            spei*nutritiondiversity +
+            spei*stability_violence,
+           data=all)
 
 summary(mod)
 
-rasts <- makeRasts(mod)
+#Dry
+plot(makeRasts(mod, "Dry")[[2]])
 
-#wet_censored
-plot(rasts[[2]])
+#Wet
+plot(makeRasts(mod, "Wet")[[2]])
 
-#dry_censored
-plot(rasts[[4]])
-
-#dry
-plot(rasts[[3]])
-
-#wet
-plot(rasts[[1]])
 
 
 writeRaster(rasts[[1]], 'Wet.tif', format='GTiff', overwrite=T)
 writeRaster(rasts[[3]], 'Dry.tif', format='GTiff', overwrite=T)
 
 
-plot(dry, main='Expected Change in HAZ Scores From a 24-Month SPEI of < -1.5',
-     xlim=c(-100, 150), ylim=c(-40, 50), axes=F)
-plot(wet, main='Expected Change in HAZ Scores From a 24-Month SPEI of > 1.5',
-     xlim=c(-100, 150), ylim=c(-40, 50), axes=F)
+library(raster)
+library(rgdal)
+
+rast <- makeRasts(mod, "Wet", usebase=TRUE)[[2]]
+sp <- readOGR('G:/My Drive/DHS Spatial Covars/Global Codes and Shapefile', 
+              'ne_50m_admin_0_countries')
+
+plot(background,
+     main='Expected Change in HAZ Scores From a 24-Month SPEI of < -1.5',
+     ext=extent(c(-100, 150, -40, 50)),
+     col='#A8A48F',
+     axes=F,
+     legend=F)
+plot(rast, 
+     add=T)
+plot(sp, add=T)
 
 
