65756f97422cb7a236fd9167b87d2e1bd70e4ef2
mcooper <mw.coop.r@gmail.com>
2018-09-21 22:14:31 +0000

    try new random forest methods

--- models/Random Forest.R
+++ models/Random Forest.R
@@ -1,12 +1,8 @@
 library(dplyr)
-library(lme4)
-library(broom)
-library(car)
 library(randomForest)
 library(doParallel)
 
 setwd('~/dhsprocessed')
-setwd('G://My Drive/DHS Processed')
 
 hha <- read.csv('HH_data_A.csv')
 spei <- read.csv('PrecipIndices.csv')
@@ -15,45 +11,63 @@ cov <- read.csv('SpatialCovars.csv')
 all <- Reduce(function(x, y){merge(x,y,all.x=T, all.y=F)}, list(hha, spei, cov)) %>%
   na.omit
 
-#Try rescaling some geospatial covariates
-all$precip_10yr_mean <- (all$precip_10yr_mean*12)/1000
-all$gdp <- all$gdp/1000
-all$market_dist <- all$market_dist/(24*7)
-all$ndvi <- all$ndvi/5000
-all$population <- all$population/1000
-all$tmax_10yr_mean <- all$tmax_10yr_mean - 273.15
-all$tmin_10yr_mean <- all$tmin_10yr_mean - 273.15
-all$builtup <- all$builtup*100
-all$elevation <- all$elevation/1000
-
-all$gdp_l <- log(all$gdp)
-
 data <- all %>%
-  select(spei24, precip_10yr_mean, 
-           forest, gdp, government_effectiveness, irrigation, market_dist, 
-           ndvi, population, stability_violence, tmax_10yr_mean, 
-           tmin_10yr_mean, crop_prod, fieldsize, nutritiondiversity, 
-           builtup, elevation, high_settle, low_settle, roughness)
+  dplyr::select(ag_pct_gdp, forest, government_effectiveness, irrig_aei, market_dist, ndvi, population,
+                stability_violence, crop_prod, fieldsize, nutritiondiversity, builtup, elevation, roughness,
+                imports_percap, grid_gdp, grid_hdi, precip_10yr_mean, tmin_10yr_mean, tmax_10yr_mean)
 
-residuals <- lm(haz_dhs ~ age + as.factor(calc_birthmonth) + 
-                      birth_order + hhsize + sex + mother_years_ed + toilet +
-                      head_age + head_sex + wealth_index, data=all) %>%
-  residuals
+haz <- all$haz_dhs
 
 
-
-cl <- makeCluster(4, outfile = '')
+cl <- makeCluster(8, outfile = '')
 registerDoParallel(cl)
 
 
-start <- Sys.time()
-rf <- foreach(ntree=rep(15, 4), .combine=combine, .multicombine=TRUE,
-              .packages='randomForest') %dopar% {
-    randomForest(residuals ~ ., data=data, ntree=ntree)
+for (i in c("loess", "ones")){
+  
+  if(i=='loess'){
+    category <- ifelse(all$spei24 > 1.5, "Wet",
+                       ifelse(all$spei24 < -0.4, "Dry", "Normal"))
+  }
+  
+  if(i=='ones'){
+    category <- ifelse(all$spei24 > 1, "Wet",
+                       ifelse(all$spei24 < -1, "Dry", "Normal"))
+  }
+  
+  if(i=='onepointfive'){
+    category <- ifelse(all$spei24 > 1.5, "Wet",
+                       ifelse(all$spei24 < -1.5, "Dry", "Normal"))
+  }
+  
+  if(i=='two'){
+    category <- ifelse(all$spei24 > 2, "Wet",
+                       ifelse(all$spei24 < -2, "Dry", "Normal"))
+  }
+  
+  #wet
+  rfwet <- foreach(n=seq(1,8), .combine=combine, .multicombine=TRUE, .packages='randomForest') %dopar% {
+    mod <- randomForest(haz[category=='Wet'] ~ ., data=data[category=='Wet', ], ntree=65)
+    mod
+  }
+  save('rfwet', file=paste0('rf-haz-wet', i))
+  
+  #dry
+  rfdry <- foreach(n=seq(1,8), .combine=combine, .multicombine=TRUE, .packages='randomForest') %dopar% {
+    mod <- randomForest(haz[category=='Dry'] ~ ., data=data[category=='Dry', ], ntree=65)
+    mod
+  }
+  save('rfdry', file=paste0('rf-haz-dry', i))
+  
+  #normal
+  rfnormal <- foreach(n=seq(1,8), .combine=combine, .multicombine=TRUE, .packages='randomForest') %dopar% {
+    mod <- randomForest(haz[category=='Normal'] ~ ., data=data[category=="Normal", ], ntree=65)
+    mod
+  }
+  save('rfnormal', file=paste0('rf-haz-normal', i))
+  
 }
-Sys.time() - start
 
-setwd('~')
+system('./../telegram.sh')
 
-system('./telegram.sh')
 
