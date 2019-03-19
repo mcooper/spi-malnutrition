c4f326bb42e3788dfa22144c0fb8cf9be5c581c8
mcooper <mw.coop.r@gmail.com>
2019-03-19 13:59:55 -0400

    final clean repo

--- scope/Compare ESA-CCI vs Globeland30.js
+++
@@ -1,7 +0,0 @@
-var gl00 = ee.ImageCollection('users/mwcoopr/globeland30_2010').mosaic()
-
-var lc00 = ee.Image("users/geflanddegradation/lcov/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2010-v207")
-
-Map.addLayer(gl00.eq(10));
-
-Map.addLayer(lc00.gte(10).and(lc00.lte(30)))
